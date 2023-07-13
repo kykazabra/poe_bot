9ea7ba23135d10a81bb2064ed331fb8888d9d380
Andrew <kykazabra@gmail.com>
2023-07-13 23:40:02 +0300

    Поправил структуру файла с ценами

--- calc_prices.py
+++ calc_prices.py
@@ -5,7 +5,7 @@ import requests
 league = "Crucible"  # ввод лиги
 r = requests.get(f'https://poe.ninja/api/data/currencyoverview?league={league}&type=Currency').json()
 
-items_prices = []
+items_prices = {}
 
 for item in r['lines']:
     if 'receive' in item.keys():
@@ -13,7 +13,7 @@ for item in r['lines']:
         value_buy = item['receive']['value']
         value_sell = item['chaosEquivalent']
 
-        items_prices.append({'item_name': item_name, 'price_buy': value_buy, 'price_sell': value_sell})
+        items_prices[item_name] = {'price_buy': value_buy, 'price_sell': value_sell}
 
 with open(r'data\items_prices.json', 'w') as f:
     json.dump(items_prices, f, indent=4)
